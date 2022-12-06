a0e9893f74d3ed285b92b718dcf31578871f9155
MichaelArthur4 <103616045+MichaelArthur4@users.noreply.github.com>
2022-12-05 23:38:51 -0500

    trying to delete wtf
    node modules...

--- clean_project/client/src/pages/FriendsListPage.js
+++
@@ -1,9 +0,0 @@
-import FriendList from "../components/friends/FriendList"
-
-function FriendsListPage(){
-    return(
-        <div>Friends List Page
-        <FriendList /></div>
-    )
-}
-export default FriendsListPage
